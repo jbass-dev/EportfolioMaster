8a94af37710cb2e8df26e824f1968d897d4d8415
babyboy198331-coder <babyboy198331@gmail.com>
2026-06-12 21:52:28 -0500

    removed  the light/dark mode

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -19,17 +19,7 @@ export default function Nav({ toggleModal, toggleContrast }) {
             About
           </a>
         </li>
-        <li className="nav__link" onClick={toggleContrast}>
-          <a
-            href="#"
-            className="nav__link--anchor link__hover-effect link__hover-effect--black"
-            aria-label="Toggle light mode"
-            onClick={(e) => e.preventDefault()}
-          >
-            <i className="fa-solid fa-circle-half-stroke" aria-hidden="true"></i>
-          </a>
-        </li>
-        <li className="nav__link click">
+<li className="nav__link click">
           <button className="nav__cta" onClick={toggleModal}>
             Say Hi ↗
           </button>
